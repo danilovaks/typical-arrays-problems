b1bd000539b3cb720cc0cf63ea220faad680b196
danilovaks <49689868+danilovaks@users.noreply.github.com>
2020-09-30 21:48:49 +0300

    init: start task typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-
 exports.min = function min (array) {
-  return 0;
+  Math.min.apply(Math, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  for(let i = 0; i < array.length; i++) {
+      sum += array[i];
+  }
+  return sum / array.length;
 }
